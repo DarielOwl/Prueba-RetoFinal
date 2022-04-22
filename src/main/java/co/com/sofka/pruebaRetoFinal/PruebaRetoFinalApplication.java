54742f103e6657c9f0a3dd1c9d57c1444d1e6fdc
DarielOWL <darieldesosa@gmail.com>
2022-04-22 20:42:14 -0300

    Proyecto Base, Se conecto a una BD en MongoAtlas

---
+++ src/main/java/co/com/sofka/pruebaRetoFinal/PruebaRetoFinalApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.pruebaRetoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaRetoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaRetoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/pruebaRetoFinal/PruebaRetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.pruebaRetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaRetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
