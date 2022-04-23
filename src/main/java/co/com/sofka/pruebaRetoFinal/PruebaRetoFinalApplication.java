7441a7e87925fb41c76b84e734dc954a933f5514
federzvz <85709865+federzvz@users.noreply.github.com>
2022-04-22 23:34:24 -0300

    Swagger using Java11

--- src/main/java/co/com/sofka/pruebaRetoFinal/PruebaRetoFinalApplication.java
+++ src/main/java/co/com/sofka/pruebaRetoFinal/PruebaRetoFinalApplication.java
@@ -2,9 +2,26 @@ package co.com.sofka.pruebaRetoFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
+@EnableSwagger2
 public class PruebaRetoFinalApplication {
+	@Bean
+	public Docket classGateAPI(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaRetoFinalApplication.class, args);
