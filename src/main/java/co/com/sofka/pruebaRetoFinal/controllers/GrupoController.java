ca9ee7fd5eb4da9fe1b79a2fac43beb9bbb49c3e
DarielOWL <darieldesosa@gmail.com>
2022-04-29 00:51:52 -0300

    Añadir notas a Estudiantes de un Grupo

--- src/main/java/co/com/sofka/pruebaRetoFinal/controllers/GrupoController.java
+++ src/main/java/co/com/sofka/pruebaRetoFinal/controllers/GrupoController.java
@@ -7,6 +7,7 @@ import co.com.sofka.pruebaRetoFinal.services.Impl.EstudianteServiceImpl;
 import co.com.sofka.pruebaRetoFinal.services.Impl.GrupoServiceImpl;
 import co.com.sofka.pruebaRetoFinal.services.Impl.MaestroServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.BooleanOperators;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -50,7 +51,7 @@ public class GrupoController {
     //Actualizar Grupo
     @PutMapping("/updateGrupo/{id}")
     private Mono<Grupo> update(@PathVariable("id") String id, @RequestBody Grupo grupo) {
-        try{
+        try {
             Grupo grupo1 = this.grupoService.findById(id).block();
             grupo1.setEstado(true);
             grupo1.setNombre(grupo.getNombre());
@@ -58,8 +59,8 @@ public class GrupoController {
             grupo1.setCurso(grupo.getCurso());
             return this.grupoService.update(id, grupo1)
                     .flatMap(grupo2 -> Mono.just(grupo1)).switchIfEmpty(Mono.empty());
-        }catch (Exception e){
-           return Mono.error(new Throwable("No existe el grupo con la id"+id));
+        } catch (Exception e) {
+            return Mono.error(new Throwable("No existe el grupo con la id" + id));
         }
 
     }
@@ -75,13 +76,13 @@ public class GrupoController {
 
     //Obtener grupos con estado TRUE y con todos sus datos incluida la lista de estudiantes.
     @GetMapping("/allGruposActivos")
-    private Flux<Grupo> allGruposActivos(){
-        return this.grupoService.findAll().filter(g -> g.isEstado()!=false);
+    private Flux<Grupo> allGruposActivos() {
+        return this.grupoService.findAll().filter(g -> g.isEstado() != false);
     }
 
     //Borrar grupo de forma lógica mediante su ID
     @PutMapping("/deleteGrupo/{id}")
-    private Mono<Grupo> logicDeleteGrupoById(@PathVariable("id") String id){
+    private Mono<Grupo> logicDeleteGrupoById(@PathVariable("id") String id) {
         Grupo grupo = this.grupoService.findById(id).block();
         grupo.setEstado(false);
         return this.grupoService.save(grupo);
@@ -89,29 +90,29 @@ public class GrupoController {
 
     //Eliminar estudiante de un grupo mediante ID estudiante e ID grupo.
     @PutMapping("/deleteEstudianteFromGrupo/{idEstudiante}/{idGrupo}")
-    private Mono<Estudiante> deleteEstudianteFromGrupo(@PathVariable("idEstudiante") String idEstudiante,@PathVariable("idGrupo") String idGrupo){
-        try{
+    private Mono<Estudiante> deleteEstudianteFromGrupo(@PathVariable("idEstudiante") String idEstudiante, @PathVariable("idGrupo") String idGrupo) {
+        try {
             Grupo grupo = this.grupoService.findById(idGrupo).block();
             Estudiante estudiante = this.estudianteService.findById(idEstudiante).block();
             List<Estudiante> estudianteList = grupo.getEstudiantes();
             Iterator iterator = estudianteList.iterator();
-            while(iterator.hasNext()){
-                Estudiante e =(Estudiante) iterator.next();
-                if(e.getId().equalsIgnoreCase(idEstudiante)){
+            while (iterator.hasNext()) {
+                Estudiante e = (Estudiante) iterator.next();
+                if (e.getId().equalsIgnoreCase(idEstudiante)) {
                     iterator.remove();
                 }
             }
             estudiante.setGrupo("");
-            return this.grupoService.update(idGrupo,grupo).then(this.estudianteService.save(estudiante));
-        }catch (Exception e){
+            return this.grupoService.update(idGrupo, grupo).then(this.estudianteService.save(estudiante));
+        } catch (Exception e) {
             return Mono.empty();
         }
     }
 
     //Agregar estudiante a un grupo mediante ID estudiante e ID grupo.
     @PutMapping("/addEstudianteToGrupo/{idEstudiante}/{idGrupo}")
-    private Mono<Estudiante> addEstudianteToGrupo(@PathVariable("idEstudiante") String idDocumentoIdentidad,@PathVariable("idGrupo") String idGrupo){
-        try{
+    private Mono<Estudiante> addEstudianteToGrupo(@PathVariable("idEstudiante") String idDocumentoIdentidad, @PathVariable("idGrupo") String idGrupo) {
+        try {
             Grupo grupo = this.grupoService.findById(idGrupo).block();
             Estudiante estudiante = this.estudianteService.findById(idDocumentoIdentidad).block();
             estudiante.setGrupo(grupo.getId());
@@ -119,7 +120,7 @@ public class GrupoController {
             estudianteList.add(estudiante);
             grupo.setEstudiantes(estudianteList);
             return this.grupoService.save(grupo).then(this.estudianteService.save(estudiante));
-        }catch (Exception e){
+        } catch (Exception e) {
             Grupo grupo = this.grupoService.findById(idGrupo).block();
             Estudiante estudiante = this.estudianteService.findById(idDocumentoIdentidad).block();
             estudiante.setGrupo(grupo.getId());
@@ -132,10 +133,10 @@ public class GrupoController {
 
     //Obtener lista de estudiantes de un grupo mediante el ID del grupo.
     @GetMapping("/allEstudiantesFromGrupo/{idGrupo}")
-    private Flux<Estudiante> allEstudiantesFromGrupo(@PathVariable("idGrupo") String id){
-        try{
+    private Flux<Estudiante> allEstudiantesFromGrupo(@PathVariable("idGrupo") String id) {
+        try {
             return Flux.fromIterable(grupoService.findById(id).block().getEstudiantes());
-        }catch (Exception e){
+        } catch (Exception e) {
             return Flux.empty();
         }
 
@@ -146,17 +147,17 @@ public class GrupoController {
 
     //Añadir horario a un Grupo de Clase-------------
     @PutMapping("/addHorarioClase/{id}")
-    private Mono<Clase> addHorarioDeClase(@PathVariable("id") String id,@RequestBody Clase clase){
+    private Mono<Clase> addHorarioDeClase(@PathVariable("id") String id, @RequestBody Clase clase) {
 
         //Primero buscamos el grupo a añadir la Clase
         Grupo grupo = this.grupoService.findById(id).block();
 
         //Si no tiene clase, añadirle una nueva
-        if (grupo.getClases() == null){
+        if (grupo.getClases() == null) {
             List<Clase> claseNueva = new ArrayList<Clase>();
             claseNueva.add(clase);
             grupo.setClases(claseNueva);
-            return this.grupoService.update(id,grupo).thenReturn(clase);
+            return this.grupoService.update(id, grupo).thenReturn(clase);
         }
 
         //Obtener el grupo y asignarle la nueva clase
@@ -165,12 +166,12 @@ public class GrupoController {
         grupo.setClases(claseNueva);
 
         //Si ya tiene grupo de clase, añade uno nuevo
-        return this.grupoService.update(id,grupo).thenReturn(clase);
+        return this.grupoService.update(id, grupo).thenReturn(clase);
     }
 
     //Listar horarios de un grupo en especifico
     @GetMapping("/allHorariosGrupos/{id}")
-    private Flux<Clase> allHorariosDeGrupos(@PathVariable("id") String id){
+    private Flux<Clase> allHorariosDeGrupos(@PathVariable("id") String id) {
         return Flux.fromIterable(this.grupoService.findById(id).block().getClases());
     }
 
@@ -180,81 +181,105 @@ public class GrupoController {
             @PathVariable("idGrupo") String idGrupo,
             @PathVariable("idMaestro") String idMaestro,
             @PathVariable("nombreMateria") String nombreMateria,
-            @RequestBody List<Horario> horarios){
-        try{
+            @RequestBody List<Horario> horarios) {
+        try {
             Grupo grupoMono = this.grupoService.findById(idGrupo).block();
             Clase clase = new Clase(
                     new Materia(nombreMateria),
                     horarios,
                     this.maestroService.findById(idMaestro).block());
-            if(grupoMono.getClases()==null){
+            if (grupoMono.getClases() == null) {
                 List<Clase> claseList = new ArrayList<Clase>();
                 claseList.add(clase);
                 grupoMono.setClases(claseList);
-                return this.grupoService.update(idGrupo,grupoMono).then(this.claseService.save(clase));
+                return this.grupoService.update(idGrupo, grupoMono).then(this.claseService.save(clase));
             }
             grupoMono.getClases().add(clase);
-            return this.grupoService.update(idGrupo,grupoMono).then(this.claseService.save(clase));
-        }catch (Exception e){
+            return this.grupoService.update(idGrupo, grupoMono).then(this.claseService.save(clase));
+        } catch (Exception e) {
             return Mono.empty();
         }
     }
 
     //URL para hacer PUT de maestro, que vincule al maestro como director de grupo a un grupo,
     @PutMapping("/addDirectorToGrupo/{idGrupo}/{idMaestro}")
-    public Mono<Maestro> addDirectorToGrupo(@PathVariable("idGrupo") String idGrupo, @PathVariable("idMaestro") String idMaestro){
-        try{
+    public Mono<Maestro> addDirectorToGrupo(@PathVariable("idGrupo") String idGrupo, @PathVariable("idMaestro") String idMaestro) {
+        try {
             Maestro maestro = this.maestroService.findById(idMaestro).block();
             Grupo grupo = this.grupoService.findById(idGrupo).block();
             maestro.setIdGrupoDirector(grupo.getId());
             grupo.setDirector(maestro);
-            return this.grupoService.update(idGrupo,grupo).then(this.maestroService.update(idMaestro,maestro));
-        }catch (Exception e){
+            return this.grupoService.update(idGrupo, grupo).then(this.maestroService.update(idMaestro, maestro));
+        } catch (Exception e) {
             return Mono.empty();
         }
     }
 
     //Eliminar director de un grupo segúin su idGrupo, idMaestro
     @PutMapping("/removeDirectorFromGrupo/{idGrupo}/{idMaestro}")
-    public Mono<Maestro> removeDirectorFromGrupo(@PathVariable("idGrupo") String idGrupo, @PathVariable("idMaestro") String idMaestro){
-        try{
+    public Mono<Maestro> removeDirectorFromGrupo(@PathVariable("idGrupo") String idGrupo, @PathVariable("idMaestro") String idMaestro) {
+        try {
             Maestro maestro = this.maestroService.findById(idMaestro).block();
             Grupo grupo = this.grupoService.findById(idGrupo).block();
             grupo.setDirector(null);
             maestro.setIdGrupoDirector("");
-            return this.grupoService.update(idGrupo,grupo).then(this.maestroService.update(idMaestro,maestro));
-        }catch (Exception e){
+            return this.grupoService.update(idGrupo, grupo).then(this.maestroService.update(idMaestro, maestro));
+        } catch (Exception e) {
             return Mono.empty();
         }
     }
 
     @GetMapping("/findGrupoFromMaestroDirector/{correo}")
-    public Mono<Grupo> findGrupoFromMaestroDirector(@PathVariable("correo") String correo){
-        try{
+    public Mono<Grupo> findGrupoFromMaestroDirector(@PathVariable("correo") String correo) {
+        try {
             return this.grupoService.findById(this.maestroService.findByCorreo(correo).block().getIdGrupoDirector())
-                    .filter(g->g.isEstado()==true);
-        }catch (Exception e){
+                    .filter(g -> g.isEstado() == true);
+        } catch (Exception e) {
             return Mono.empty();
         }
     }
 
     //Agregar nota al estudiante
     @PutMapping("/addNotaToEstudiante/{correoMaestro}/{idEstudiante}")
-    public Mono<Nota> addNotaToEstudiante(@PathVariable("correoMaestro") String correoMaestro,@PathVariable("idEstudiante") String idEstudiante, @RequestBody Nota nota){
+    public Mono<Nota> addNotaToEstudiante(@PathVariable("correoMaestro") String correoMaestro, @PathVariable("idEstudiante") String idEstudiante, @RequestBody Nota notaNueva) {
 
-        //Primero buscamos el maestro
-        Maestro maestro = this.maestroService.findByCorreo(correoMaestro).block();
+        try {
+            //Primero buscamos el maestro
 
+            Grupo grupo = this.grupoService.findById(this.maestroService.findByCorreo(correoMaestro).block().getIdGrupoDirector()).block();
 
+            //Si la lista de Estudiantes esta vacia
+            if (grupo.getEstudiantes() == null)
+                return Mono.empty();
 
-        Estudiante estudiante = this.estudianteService.findById(idEstudiante).block();
+            List<Estudiante> estudiantes = grupo.getEstudiantes(); //Obtenemos la lista de estudiantes de ese grupo
 
+            estudiantes.stream().map(estudiante -> {
 
+                //Si encuentra el estudiante, inserta la notas
+                if (estudiante.getId().equalsIgnoreCase(idEstudiante)) {
 
+                    //Si las notas estan vacias, inserta una nueva lista de Notas
+                    if (estudiante.getNotas() == null) {
+                        List<Nota> notas = new ArrayList<Nota>();
+                        notas.add(notaNueva);
+                        estudiante.setNotas(notas);
+                    }
 
-        return null;
+                    //Si ya tiene notas, le añadimos
+                    estudiante.getNotas().add(notaNueva);
+                }
+                return estudiante;
+            });
+
+            //Setiamos la lista de estudiantes a Grupo
+            grupo.setEstudiantes(estudiantes);
+            return this.grupoService.update(grupo.getId(),grupo).thenReturn(notaNueva); //Actualizamos el grupo
+
+        } catch (Exception e) {
+            return Mono.error(new Throwable("No funciono crack")); //Sino devuelve vacio chaval!!!
+        }
     }
-
 
 
 }
